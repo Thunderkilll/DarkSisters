feca89e9cf81b6f9870b60385bf350dcfb458d75
thunderkill <khaled.guesmi@esprit.tn>
2019-05-29 04:51:18 +0200

    playermovement and animation

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    public int health = 100;
+    public List<float> volumes;
+    private Scene scene;
+    public Animator animator;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    public bool death = false;
+    public bool isGameOver = false;
+    bool jump = false;
+    public static float scoreGim = 0;
+    public CharacterController2D controller;
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       if (!death)
+         {
+
+            horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+            animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+            // float verticalMove = joystick.Vertical;
+
+            if (Input.GetKey(KeyCode.Space))
+     {
+                 jump = true;
+               animator.SetBool("isJumping", jump);
+            }
+            else
+            {
+                jump = false;
+                animator.SetBool("isJumping", jump);
+            }
+
+
+
+      }
+         else
+   {
+       Debug.Log("You are dead");
+
+ }
+    
+    }
+
+    void FixedUpdate()
+    {
+        // Move our character
+
+      
+        controller.Move(horizontalMove * Time.fixedDeltaTime , false , jump);
+        jump = false;
+        animator.SetBool("isJumping", jump);
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.tag == "death")
+        {
+
+            Death(); //restart game 
+        }
+        if (col.tag == "directions")
+        {
+
+            Debug.Log("back this way player move foward and then jump");
+        }
+    }
+
+    public void Death()
+    {
+        animator.SetBool("isDead", true);
+        Debug.Log("you died ");
+    }
+
+    }
