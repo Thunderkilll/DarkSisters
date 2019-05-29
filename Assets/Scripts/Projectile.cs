17acb9bf8ae59d54346f9e899e43bf6587a91217
thunderkill <khaled.guesmi@esprit.tn>
2019-05-29 07:53:39 +0200

    ssss

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float speed;
+    public float lifeTime;
+    public GameObject destroyeffect; 
+    private void Start()
+    {
+        Invoke("DestroyProjectile",lifeTime);
+    }
+    private void Update()
+    {
+        transform.Translate(transform.up * speed * Time.deltaTime);
+    }
+
+    public void DestroyProjectile()
+    {
+        Instantiate(destroyeffect , transform.position , Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -10,9 +10,9 @@ public class Weapon : MonoBehaviour {
  
 	// Update is called once per frame
 	void Update () {
-        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float rotz = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rotz + offset);
+        //Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+      //  float rotz = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+      //  transform.rotation = Quaternion.Euler(0f, 0f, rotz + offset);
         if (Input.GetMouseButtonDown(0))
         {
             Instantiate(projectile, shootpoint.position, transform.rotation);
