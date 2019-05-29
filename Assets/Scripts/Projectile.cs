1bbd7ee60389015711c32a8d22d9a7f1a3ea19a0
thunderkill <khaled.guesmi@esprit.tn>
2019-05-29 08:22:29 +0200

    ssssss

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,19 +6,33 @@ public class Projectile : MonoBehaviour {
 
     public float speed;
     public float lifeTime;
-    public GameObject destroyeffect; 
+    public float distance;
+   // public GameObject destroyeffect;
+    public LayerMask whatIsSolid;
     private void Start()
     {
         Invoke("DestroyProjectile",lifeTime);
     }
     private void Update()
     {
-        transform.Translate(transform.up * speed * Time.deltaTime);
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right, distance);
+        if (hitInfo.collider != null)
+        {
+            if (hitInfo.collider.tag == "Enemy")
+            {
+                Debug.Log(hitInfo.collider.gameObject.name);
+                Debug.Log("give enemy a bit of damage");
+            }
+            //DestroyProjectile();
+        }
+        transform.Translate(transform.right * speed * Time.deltaTime);
+         
     }
 
     public void DestroyProjectile()
     {
-        Instantiate(destroyeffect , transform.position , Quaternion.identity);
+       // Instantiate(destroyeffect , transform.position , Quaternion.identity);
         Destroy(gameObject);
+ 
     }
 }
