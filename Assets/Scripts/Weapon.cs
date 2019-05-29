dfb9b3b42867df193d5b6e8b8df87274c98970d7
thunderkill <khaled.guesmi@esprit.tn>
2019-05-29 07:37:34 +0200

    sss

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    public float offset;
+    public GameObject projectile;
+    public Transform shootpoint; 
+ 
+	// Update is called once per frame
+	void Update () {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotz = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotz + offset);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(projectile, shootpoint.position, transform.rotation);
+        }
+	}
+}
